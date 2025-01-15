2804dcc343bcabbdadeb92223f1fd4e09f3defa6
Sarah Rocco <sarahrocco90@gmail.com>
2025-01-15 17:56:14 -0500

    create search tab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('Hi there! :)');
+const fetchData = async (searchTerm) => {
+  const response = await axios.get("http://www.omdbapi.com/", {
+    params: {
+      apikey: "24af5dc2",
+      s: searchTerm,
+    },
+  });
+
+  console.log(response.data);
+};
+
+const input = document.querySelector("input");
+
+let timeoutId;
+const onInput = (event) => {
+  if (timeoutId) {
+    clearTimeout(timeoutId);
+  }
+  timeoutId = setTimeout(() => {
+    fetchData(event.target.value);
+  }, 500);
+};
+
+input.addEventListener("input", onInput);
