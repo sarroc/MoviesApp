4fa0e7f6fd3f346b73546e4178b51e42eaf92544
Sarah Rocco <sarahrocco90@gmail.com>
2025-01-19 21:01:12 -0500

    create and apply debounce

--- index.js
+++ index.js
@@ -11,14 +11,8 @@ const fetchData = async (searchTerm) => {
 
 const input = document.querySelector("input");
 
-let timeoutId;
-const onInput = (event) => {
-  if (timeoutId) {
-    clearTimeout(timeoutId);
-  }
-  timeoutId = setTimeout(() => {
-    fetchData(event.target.value);
-  }, 500);
-};
 
-input.addEventListener("input", onInput);
+const onInput = debounce((event) => {
+  fetchData(event.target.value);
+});
+input.addEventListener("input", debounce(onInput, 500));
