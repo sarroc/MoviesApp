931f03eb0df603004f931ae86fffe19d2e13f338
Sarah Rocco <sarahrocco90@gmail.com>
2025-01-22 20:23:43 -0500

    finalize movie request with selection and statistics

--- index.js
+++ index.js
@@ -31,6 +31,11 @@ const resultsWraper = document.querySelector(".results");
 const onInput = async (event) => {
   const movies = await fetchData(event.target.value);
 
+  if (!movies.length) {
+    dropdown.classList.remove("is-active");
+    return;
+  }
+
   resultsWraper.innerHTML = "";
   dropdown.classList.add("is-active");
   for (let movie of movies) {
@@ -42,6 +47,11 @@ const onInput = async (event) => {
     <img src="${imgSrc}" />
     ${movie.Title}
     `;
+    options.addEventListener("click", () => {
+      dropdown.classList.remove("is-active");
+      input.value = movie.Title;
+      onMovieSelect(movie);
+    });
 
     resultsWraper.appendChild(options);
   }
@@ -53,3 +63,53 @@ document.addEventListener("click", (event) => {
     dropdown.classList.remove("is-active");
   }
 });
+
+const onMovieSelect = async (movie) => {
+  const response = await axios.get("http://www.omdbapi.com/", {
+    params: {
+      apikey: "24af5dc2",
+      i: movie.imdbID,
+    },
+  });
+
+  document.querySelector("#summary").innerHTML = movieTemplate(response.data);
+};
+
+const movieTemplate = (movieDetail) => {
+  return `
+  <article class='media'>
+  <figure class= 'media-left'>
+  <p class= 'image'>
+  <img src='${movieDetail.Poster}' />
+  </p>
+  </figure>
+  <div class='media-content'>
+  <div class='content'>
+<h1>${movieDetail.Title}</h1>
+<h4>${movieDetail.Genre}</h4>
+<p>${movieDetail.Plot}</p>
+  </div>
+  </div>
+  </article>
+  <article class='notification is-primary'>
+  <p class='title'>${movieDetail.Awards}</p>
+  <p class='subtitle'>Awards</p>
+  </article>
+   <article class='notification is-primary'>
+  <p class='title'>${movieDetail.BoxOffice}</p>
+  <p class='subtitle'>Box Office</p>
+  </article>
+   <article class='notification is-primary'>
+  <p class='title'>${movieDetail.Metascore}</p>
+  <p class='subtitle'>Metascore</p>
+  </article>
+   <article class='notification is-primary'>
+  <p class='title'>${movieDetail.imdbRating}</p>
+  <p class='subtitle'>IMDB Rating</p>
+  </article>
+   <article class='notification is-primary'>
+  <p class='title'>${movieDetail.imdbVotes}</p>
+  <p class='subtitle'>IMDB Votes</p>
+  </article>
+  `;
+};
