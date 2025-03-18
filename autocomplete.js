0d24190a35abad2b7de3b6b2252b6c0b8568e052
Sarah Rocco <sarahrocco90@gmail.com>
2025-03-18 21:23:07 +0100

    removing movie reference to have a reusable autocomplete function

--- autocomplete.js
+++ autocomplete.js
@@ -3,9 +3,10 @@ const createAutoComplete = ({
   renderOption,
   onOptionSelect,
   inputValue,
+  fetchData,
 }) => {
   root.innerHTML = `
-<label><b>Search For a Movie</b></label>
+<label><b>Search</b></label>
 <input class="input" />
 <div class="dropdown">
 <div class="dropdown-menu">
@@ -19,24 +20,24 @@ const createAutoComplete = ({
   const resultsWraper = root.querySelector(".results");
 
   const onInput = async (event) => {
-    const movies = await fetchData(event.target.value);
+    const items = await fetchData(event.target.value);
 
-    if (!movies.length) {
+    if (!items.length) {
       dropdown.classList.remove("is-active");
       return;
     }
 
     resultsWraper.innerHTML = "";
     dropdown.classList.add("is-active");
-    for (let movie of movies) {
+    for (let item of items) {
       const options = document.createElement("a");
 
       options.classList.add("dropdown-item");
-      options.innerHTML = renderOption(movie);
+      options.innerHTML = renderOption(item);
       options.addEventListener("click", () => {
         dropdown.classList.remove("is-active");
-        input.value = inputValue(movie);
-        onOptionSelect(movie);
+        input.value = inputValue(item);
+        onOptionSelect(item);
       });
 
       resultsWraper.appendChild(options);
