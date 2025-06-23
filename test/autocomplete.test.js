b3df9cda0a421e1495fc94041fca6d06f44b917f
Sarah Rocco <sarahrocco90@gmail.com>
2025-06-23 20:04:15 +0200

    implement waitFor function

--- test/autocomplete.test.js
+++ test/autocomplete.test.js
@@ -1,3 +1,20 @@
+const waitFor = (selector) => {
+    return new Promise((resolve, reject) => {
+        const interval = setInterval(() => {
+            if (document.querySelector(selector)) {
+                clearInterval(interval);
+                clearTimeout(timeout);
+                resolve();
+            }
+        }, 30);
+
+        const timeout = setTimeout(() => {
+            clearInterval(interval);          
+            reject();
+        }, 2000);
+    });
+};
+
 beforeEach(() => {
     document.querySelector('#target').innerHTML = '';
     createAutoComplete({
@@ -22,10 +39,12 @@ it('Dropdown starts closed', () => {
     expect(dropdown.className).not.to.include('is-active');
 });
 
-it('After seraching, dropdown opens up', () => {
+it('After seraching, dropdown opens up', async () => {
     const input = document.querySelector('input');
     input.value = 'avengers';
     input.dispatchEvent(new Event('input'));
+
+    await waitFor('.dropdown-item');
 
     const dropdown = document.querySelector('.dropdown');
 
