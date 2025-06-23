c755f2b061fb21dc76d4f9312d599f3b94bd800f
Sarah Rocco <sarahrocco90@gmail.com>
2025-06-23 18:59:17 +0200

    display fake 'api' on request submit

--- test/autocomplete.test.js
+++ test/autocomplete.test.js
@@ -1,5 +1,15 @@
 it('Shows an autocomplete!', () => {
     createAutoComplete({
-        root: document.querySelector('#target')
-    })
+        root: document.querySelector('#target'),
+        fetchData() {
+            return [
+                { Title: 'Avengers' },
+                { Title: 'Lion King' },
+                { Title: 'Some other movie' }
+            ];
+        },
+        renderOption(movie) {
+          return movie.Title;  
+        }
+    });
 });
